a64b5f230d678fa5064269700a304e3e32f8ea29
Åsa Jonsson <54078709+AsaExit@users.noreply.github.com>
2021-03-09 12:41:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //MONGODB:'mongodb+srv://RunExit:GeneralCaptain6471@acemille.42kav.azure.mongodb.net/mydosdb?retryWrites=true&w=majority',
+ 
   SECRET_KEY: 'personaldaylogkey'
 };
